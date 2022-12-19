df56992550a3368a48af6d2fadaf36e79b36b9cf
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2022-12-19 18:15:08 +0200

    First Commit

---
+++ JavaProject/src/FirstClass.java
@@ -0,0 +1,8 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println();
+        System.out.print("Hello World");
+        System.out.print("Hello World");
+    }
+}
