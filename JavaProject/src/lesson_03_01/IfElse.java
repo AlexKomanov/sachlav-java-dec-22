289d1304415b39346c1d3ef2f1930c384cf49daf
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2022-12-26 20:55:35 +0200

    Adding Ternary operator

--- JavaProject/src/lesson_03_01/IfElse.java
+++ JavaProject/src/lesson_03_01/IfElse.java
@@ -9,15 +9,20 @@ public class IfElse {
 
         System.out.println("Please enter your age... ");
         int age = input.nextInt();
+        String whoAreYou;
 
-        if (age == 18) {
-            System.out.println("You're 18 years old");
-        }
-        else if (age > 18) {
-            System.out.println("You're adult");
-        }
-        else {
-            System.out.println("You're young");
-        }
+//        if (age == 18) {
+//            System.out.println("You're 18 years old");
+//        }
+//        else if (age > 18) {
+//            System.out.println("You're adult");
+//        }
+//        else {
+//            System.out.println("You're young");
+//        }
+
+
+        whoAreYou = (age == 18) ? "You're 18 years old" : (age > 18) ? "You're adult" : "You're young";
+        System.out.println(whoAreYou);
     }
 }
