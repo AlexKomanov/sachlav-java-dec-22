3897e3158fdcb9454c432349af40367fe06061fa
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2022-12-28 18:48:09 +0200

    Logical Operators

---
+++ JavaProject/src/lesson_04/FalseOperator.java
@@ -0,0 +1,12 @@
+package lesson_04;
+
+public class FalseOperator {
+    public static void main(String[] args) {
+
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("!a(true) = " + !a); // !a(true)  -> false
+        System.out.println("!b(false) = " + !b); // !b(false) -> true
+    }
+}
