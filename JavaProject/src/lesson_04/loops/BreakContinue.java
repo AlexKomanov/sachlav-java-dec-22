a57cb4a0973af4cd43026e0b1ac53c555deae608
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2022-12-28 21:57:19 +0200

    Last tasks

---
+++ JavaProject/src/lesson_04/loops/BreakContinue.java
@@ -0,0 +1,24 @@
+package lesson_04.loops;
+
+public class BreakContinue {
+
+    public static void main(String[] args) {
+
+        var a = 0;
+
+        while (a < 20) {
+            a++;
+
+            if (a % 5 == 0) {
+                continue;
+            }
+
+            if (a == 17) {
+                break;
+            }
+
+            System.out.print(a + " ");
+
+        }
+    }
+}
