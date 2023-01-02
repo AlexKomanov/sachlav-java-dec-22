55c573318cde602d6b6887c714b30b86c369df47
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-01-02 18:10:13 +0200

    Adding RandomValues.java

---
+++ JavaProject/src/lesson_5/RandomValues.java
@@ -0,0 +1,28 @@
+package lesson_5;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomValues {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        ThreadLocalRandom threadLocalRandom = ThreadLocalRandom.current();
+
+        int randomInteger = random.nextInt();
+        int randomIntegerWithBound = random.nextInt(10);
+        double randomDouble = random.nextDouble();
+
+        System.out.println("randomInteger = " + randomInteger);
+        System.out.println("randomDouble = " + randomDouble);
+        System.out.println("randomIntegerWithBound = " + randomIntegerWithBound);
+
+        int randomIntegerWithBounds = threadLocalRandom.nextInt(-100, 20);
+        System.out.println("randomIntegerWithBounds = " + randomIntegerWithBounds);
+
+    }
+
+
+
+
+}
