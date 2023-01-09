5ecb138b26c4c37f8cdf004869f89887176591f4
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-01-09 15:30:43 +0200

    adding home tasks of prevous lesson

---
+++ JavaProject/src/lesson_5/homeTasks/Duplicate.java
@@ -0,0 +1,27 @@
+package lesson_5.homeTasks;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+public class Duplicate {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Random random = new Random();
+        HashMap<Integer, Integer> count = new HashMap<>();
+        for (int i = 0; i < 100; i++) {
+            numbers.add(random.nextInt(50));
+        }
+        for (int num : numbers) {
+            if (count.containsKey(num)){
+                count.put(num,(count.get(num) + 1));
+            }
+            else {
+                count.put(num,1);
+            }
+        }
+        System.out.println("Amount of unique numbers " + count.size() + " in the list.Here is their breakdown." + "Amount of not unique numbers is " + (numbers.size()- count.size()));
+        count.forEach((unicueNumber,number)-> System.out.println(unicueNumber + " number is in the list " + number + " times"));
+    }
+}
