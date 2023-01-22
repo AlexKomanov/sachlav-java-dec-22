b4a95cc03a4a61ab8924e850b6b2af11774b6891
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-01-22 17:40:19 +0200

    Added Shape.java class

---
+++ JavaProject/src/lesson_10/Shape.java
@@ -0,0 +1,20 @@
+package lesson_10;
+
+public abstract class Shape {
+
+    private String color;
+
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+
+    public abstract double calculateArea();
+
+    public abstract  double calculatePerimeter();
+
+}
