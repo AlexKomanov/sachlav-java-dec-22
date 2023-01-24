4fde0100066c30fd1d6e4d70c0c7c84278448afe
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-01-24 21:02:40 +0200

    Adding class task

---
+++ JavaProject/src/lesson_11/vehicles/Bus.java
@@ -0,0 +1,12 @@
+package lesson_11.vehicles;
+
+public class Bus extends Vehicle {
+    public Bus(String brand, int year) {
+        super(brand, year);
+    }
+
+    @Override
+    public void whoAreYou() {
+        System.out.println("I'm a bus " + getBrand() + " " + getYear() + ".");
+    }
+}
---
+++ JavaProject/src/lesson_11/vehicles/Car.java
@@ -0,0 +1,13 @@
+package lesson_11.vehicles;
+
+public class Car extends Vehicle {
+
+    public Car(String brand, int year) {
+        super(brand, year);
+    }
+
+    @Override
+    public void whoAreYou() {
+        System.out.println("I'm a car " + getBrand() + " " + getYear());
+    }
+}
