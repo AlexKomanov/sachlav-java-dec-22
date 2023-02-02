b9c717c615a56128d118b568ce15507e3b07ff3d
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-02-02 20:33:49 +0200

    error exceptions

---
+++ JavaProject/src/lesson_13/errors/Errors_01_StackOverFLowError.java
@@ -0,0 +1,12 @@
+package lesson_13.errors;
+
+public class Errors_01_StackOverFLowError {
+    public static void main(String[] args) {
+
+        calculate(10);
+    }
+
+    public static int calculate(int number) {
+        return number * calculate(number - 1);
+    }
+}
